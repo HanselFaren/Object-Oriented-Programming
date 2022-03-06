cffbdaf9f6734364ae8bfea37e6c3e40a408c917
HanselFaren <91465998+HanselFaren@users.noreply.github.com>
2022-03-06 22:59:01 +0700

    Add files via upload

---
+++ Drills(Forum)/number12.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class number12{
+	public static void main(String[] args){
+		System.out.println("number \t square \t cube");
+		
+		for(int a = 0; x <= 10 ; a++){
+			System.out.println(a + "\t" + (int)Math.pow(a,2) + "\t" + (int)Math.pow(a,3));
+		}
+	}
+}
