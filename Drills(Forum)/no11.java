c9816c90774f02d6ce62d6fb03df14a0ff4f97a4
HanselFaren <91465998+HanselFaren@users.noreply.github.com>
2022-03-06 22:42:12 +0700

    Add files via upload

---
+++ Drills(Forum)/no11.java
@@ -0,0 +1,12 @@
+public class number11{
+	public static void main(String[] args){
+		int a,b,c;
+		a = 7;
+		b = 3;
+		System.out.println("a = " + a + ", b = " + b);
+		c = a;
+		a = b;
+		b = c;
+		System.out.println("a = " + a + ", b = " + b);
+	}
+}
